061e6d1dc9d710c30631243cbb07c106aa5faa75
Laurent <laurentahsue888@gmail.com>
2021-11-16 13:23:27 +0800

    Homework Submission

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "70188bbd8f5bedb96a8333f77da39554"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB9V05KtnPGQZyX21VNwzjXsP2K8rLwbL0"
